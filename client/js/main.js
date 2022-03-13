e9a2dcf796fbf3602e03a42dc4ad848bc3053b11
Jacob Fliss <newgmobile@gmail.com>
2022-03-13 13:47:30 -0400

    admin date filtering works

--- client/js/main.js
+++ client/js/main.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
-    $("#starttime").val(new Date().toISOString().substr(0, 10));
-    $("#endtime").val(new Date().toISOString().substr(0, 10));
+    $('#starttime').val(new Date().toISOString().substr(0, 10));
+    $('#endtime').val(new Date().toISOString().substr(0, 10));
     FetchData();
-});
 
-$( "#starttime" ).change(function() {
-    FetchData();
-});
-$( "#endtime" ).change(function() {
-    FetchData();
+    $( '#starttime' ).change(function() {
+        console.log("start time changed");
+        FetchData();
+    });
+    $( '#endtime' ).change(function() {
+        console.log("end time changed");
+        FetchData();
+    });
 });
 
 function FetchData() {
+    console.log("starttime=" + document.getElementById("starttime").value + " endtime=" + document.getElementById("endtime").value);
     $.ajax({
-        url: APIURL + "/punches?starttime=" + document.getElementById("starttime") + "&endtime=" + document.getElementById("endtime"),
+        url: APIURL + "/punches?starttime=" + document.getElementById("starttime").value + "&endtime=" + document.getElementById("endtime").value,
         type: 'GET',
         dataType: "json",
         success: DisplayData
