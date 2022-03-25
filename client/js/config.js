0f3dd1a81d53f9736870bc32638026c8afde70f7
Jake <newgmobile@gmail.com>
2022-03-25 09:28:50 -0400

    set a min/max for emp ids

--- client/js/config.js
+++ client/js/config.js
@@ -1,3 +1,6 @@
 const BACKEND_PORT = 38001;
 const BACKEND_URL = location.hostname; // change to "http://server.local"
 const APIURL = "//" + BACKEND_URL + ":" + BACKEND_PORT.toString();
+
+const MIN_EMPID_CHARS = 3;
+const MAX_EMPID_CHARS = 4;
